d87b7aa2c086e117f29ec24d5f61f2ee6182757b
Narottam Goyal <narottamgoyal66@gmail.com>
2021-07-04 17:25:08 +0530

    Added      AdminPortalService/AdminPortalService.sln Added      AdminPortalService/AdminPortalService/AdminPortalService.csproj Added      AdminPortalService/AdminPortalService/Controllers/SupperAdminController.cs Added      AdminPortalService/AdminPortalService/Controllers/TenantAdminController.cs Added      AdminPortalService/AdminPortalService/Program.cs Added      AdminPortalService/AdminPortalService/Properties/launchSettings.json Added      AdminPortalService/AdminPortalService/Startup.cs Added      AdminPortalService/AdminPortalService/appsettings.Development.json Added      AdminPortalService/AdminPortalService/appsettings.json

---
+++ AdminPortalService/AdminPortalService/Controllers/TenantAdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPortalService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TenantAdminController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return StatusCode(StatusCodes.Status200OK, "Its running...");
+        }
+    }
+}
